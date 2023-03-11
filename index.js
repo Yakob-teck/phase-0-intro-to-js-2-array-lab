4160d84587ffdde0f506d1c3718bfdc0e70336f8
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-10 17:17:02 -0700

    message

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis","Gorfield"]; 
+function destructivelyAppendCat(name){
+   
+   return  cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+  return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+    function destructivelyRemoveFirstCat(name){
+
+        return cats.shift(name);
+}
+
+function appendCat(name){
+    const copyCats = [...cats, "Tom"];
+    
+return cats;
+}
+function prependCat(name){
+    const copyCats= ["Tom", ...cats]; 
+    return copyCats;
+}
+function removeLastCat(){
+   
+    const copyCats =cats.slice(0,cats.length-1);
+    return copyCats;
+}
+function removeFirstCat(){
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
