d547b9a4245f4e38437df4d53e4213ca339e7f51
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-14 14:40:04 -0600

    arrayLab

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
-const cats = ["Milo", "Otis","Gorfield"]; 
+const cats=["Milo", "Otis", "Garfield"];
+/*function name(){
+     cats.length = 0;
+     return cats.push(); 
+}*/
+
 function destructivelyAppendCat(name){
    
-   return  cats.push(name);
+    return cats.push(name);
+    
 }
 
 function destructivelyPrependCat(name){
+  
   return cats.unshift(name);
+  
 }
 function destructivelyRemoveLastCat(name){
     return cats.pop(name);
@@ -17,13 +25,15 @@ function destructivelyRemoveLastCat(name){
 }
 
 function appendCat(name){
-    const copyCats = [...cats, "Tom"];
-    
-return cats;
+    const copyCats = [...cats, "Broom"];
+return copyCats;
 }
+
 function prependCat(name){
-    const copyCats= ["Tom", ...cats]; 
+
+    const copyCats= ["Arnold", ...cats]; 
     return copyCats;
+    
 }
 function removeLastCat(){
    
